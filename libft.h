3da6c56e59fa2545c922105cb8302859dec55c4c
fenix144 <yan_1731@hotmail.com>
2022-04-12 11:48:22 -0400

    let's go

--- libft.h
+++ libft.h
@@ -136,5 +136,6 @@ void	printchar(int c, int *res);
 void	ft_putnbr_base(unsigned int n, char *base, int *res);
 char    *get_next_line(int fd);
 char	*ft_strjoinfree(char const *s1, char const *s2);
+int		ft_strchrint(const char *s, int c);
 
 #endif
